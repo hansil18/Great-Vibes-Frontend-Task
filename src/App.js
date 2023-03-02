8b7bee3b5fb0b73da480063ee20da50d08578699
hansil18 <hansiljasoliya@gmail.com>
2023-03-02 23:44:13 +0530

    initial app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='text-4xl text-red-400'>Hansil</div>
     </div>
   );
 }
